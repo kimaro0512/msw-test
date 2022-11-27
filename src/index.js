cd86a5354dce4208187c07768af8f78694ec55bc
kimaro0512 <kimaro0512@gmail.com>
2022-11-27 23:58:58 +0900

    Add msw

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { worker } from "./mocks/worker";
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
